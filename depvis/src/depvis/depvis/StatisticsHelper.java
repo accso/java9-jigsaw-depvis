cf2139981eb0f423acbf6a9ea9087508ff56d276
Martin Lehmann <martin.lehmann@accso.de>
2016-08-24 15:39:42 +0200

    DepVis Java 9 Jigsaw Module Dependency Visualizer, V0.1

--- depvis/src/depvis/depvis/StatisticsHelper.java
+++ depvis/src/depvis/depvis/StatisticsHelper.java
@@ -4,7 +4,7 @@ package depvis;
  * DepVis visualization tool for Java 9 Jigsaw modules
  * see https://github.com/accso/java9-jigsaw-depvis
  * 
- * Helper class which keeps track of some statistics, i.e. counts modules, filtered, system etc.
+ * Helper class which keeps track of some statistics, i.e. counts modules and relations (filter on/off).
  */
 class StatisticsHelper {
     static long start;
@@ -14,18 +14,18 @@ class StatisticsHelper {
     }
 
     // statistic counters
-    static int modTotalCounter = 0;		    	// number of own modules found
-    static int modCounter = 0;				      // number of modules found (filtered)
-    static int modSystemTotalCounter = 0;	  // number of modules from system found
-    static int modSystemCounter = 0;		    // number of modules from system found (filtered)
-    static int reqTotalCounter = 0;			    // number of requires connections found
-    static int reqCounter = 0;				      // number of requires connections found
-    static int reqTotalMandatedCounter = 0;	// number of requires mandated connections found
-    static int reqMandatedCounter = 0;		  // number of requires mandated connections found
-    static int reqPublicTotalCounter = 0;	  // number of requires public   connections found
-    static int reqPublicCounter = 0;		    // number of requires public   connections found (no duplicates)
-    static int exportsToTotalCounter = 0;	  // number of exportsTo connections found
-    static int exportsToCounter = 0;		    // number of exportsTo connections found (no duplicates)
+    static int modTotalCounter = 0;              // number of modules found
+    static int modCounter = 0;                   // number of modules found (filtered)
+    static int modSystemTotalCounter = 0;        // number of system modules found
+    static int modSystemCounter = 0;             // number of system modules found (filtered)
+    static int reqTotalCounter = 0;              // number of requires relations found
+    static int reqCounter = 0;                   // number of requires relations found
+    static int reqTotalMandatedCounter = 0;      // number of requires mandated relations found
+    static int reqMandatedCounter = 0;           // number of requires mandated relations found (filtered, no duplicates)
+    static int reqPublicTotalCounter = 0;        // number of requires public   relations found
+    static int reqPublicCounter = 0;             // number of requires public   relations found (filtered, no duplicates)
+    static int exportsToTotalCounter = 0;        // number of exportsTo relations found
+    static int exportsToCounter = 0;             // number of exportsTo relations found (filtered, no duplicates)
 
     // print out statistics to STDOUT
     static void printStatistics() {
