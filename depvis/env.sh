6ba3aff4a3f5970d786b9814da71e1b572be5d98
Martin Lehmann <martin.lehmann@accso.de>
2016-08-26 14:14:32 +0200

    DepVis Java 9 Jigsaw Module Dependency Visualizer, V0.1

--- depvis/env.sh
+++ depvis/env.sh
@@ -1,1 +1,20 @@
-myenv.sh
+# DepVis environment settings
+
+# configure paths here
+JAVA_HOME=TODO/path/to/java/home/goes/here
+GRAPHVIZ_HOME=TODO/path/to/graphviz2.38/home/goes/here
+
+# ---------------------------------------------------------
+# no need to change anything beyond this line
+
+PATH=$JAVA_HOME/bin:$PATH 
+
+DEPVIS_VERSION=0.1
+DEPVIS_JAR=depvis-${DEPVIS_VERSION}.jar
+
+DEPVIS_MOD_PATH=./mods
+DEPVIS_MODULE_PATH=./mlib
+DEPVIS_AUTOMATIC_MODULE_PATH=./amlib
+
+GRAPHVIZ_API_VERSION=1.2.1
+GRAPHVIZ_API_JAR=graphviz-api-${GRAPHVIZ_API_VERSION}.jar
