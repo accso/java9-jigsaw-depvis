deee485eebd7c05cf82c84c2f18bd1489657aeea
Martin Lehmann <martin.lehmann@accso.de>
2017-10-20 12:19:54 +0200

    Changed -m option to --module

--- depvis/run-print.sh
+++ depvis/run-print.sh
@@ -3,4 +3,4 @@
 # Config properties file
 PROPS_FILENAME=depvis.properties
 
-$JAVA_HOME/bin/java --module-path ${DEPVIS_MODULE_PATH}\;${DEPVIS_AUTOMATIC_MODULE_PATH} -m depvis/depvis.JigsawDepPrinter ${PROPS_FILENAME}
+$JAVA_HOME/bin/java --module-path ${DEPVIS_MODULE_PATH}\;${DEPVIS_AUTOMATIC_MODULE_PATH} --module depvis/depvis.JigsawDepPrinter ${PROPS_FILENAME}
